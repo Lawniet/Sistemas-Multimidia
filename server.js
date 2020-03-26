501a24ac65be206c2ff8d17c1f70179b46af4b2a
Lauany Reis da Silva <laulauany@hotmail.com>
2020-03-26 16:06:47 +0000

    Automatização da porta do heroku

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require("http");
+t http = require("http");
 const express = require("express");
 const app = express();
 
@@ -6,4 +6,4 @@ app.get("/", function(req, res) {
     res.send("<h1>Servidor rodando com ExpressJS</h1>");
 });
 
-http.createServer(app).listen(5000, () => console.log("Servidor rodando local na porta 3000"));
+http.createServer(app).listen(process.env.PORT || 5000, () => console.log("Servidor rodando local na porta 5000"));
