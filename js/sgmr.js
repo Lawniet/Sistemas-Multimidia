73660e1eca4f824638aac9e031822060e7f03928
Lauany Reis da Silva <laulauany@hotmail.com>
2020-03-26 14:45:39 +0000

    Atualização de estilo e acréscimo do arquivo javascript

---
+++ js/sgmr.js
@@ -0,0 +1,9 @@
+function createCanvas(app, width, height){
+}
+
+window.onload = () => {
+    const app = document.getElementById('main');
+    const gameWindow = createCanvas(app, 513, 513);
+    const gameContext = gameWindow.getContext('2d');
+    const gameDiv = 20;
+}
